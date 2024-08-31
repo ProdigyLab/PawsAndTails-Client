bd066a5dd9186b7fa238ba97a6690dedb5fe83f6
SudiptaAdhikaryJoy <sudiptaadhikaryjoy@gmail.com>
2024-08-31 12:22:20 +0600

    fix build error

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,5 @@
 import NextAuth from "next-auth";
-import type { NextAuthOptions, Session } from "next-auth";
+import type { NextAuthOptions } from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
 import FacebookProvider from "next-auth/providers/facebook";
