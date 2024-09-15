33c7d7f3331d03c57c00855d7c15cb66d0fc3410
SudiptaAdhikaryJoy <sudiptaadhikaryjoy@gmail.com>
2024-09-15 23:39:15 +0600

    feat: login page design completed with lottie animation and validations

--- src/components/auth/login/loginComponent.tsx
+++ src/components/auth/login/loginComponent.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { handleChangeLoginInput } from "@/redux/actions/loginActions";
 import { UseAppDispatch, UseAppSelector } from "@/redux/hook";
 import axios from "axios";
@@ -7,24 +7,65 @@ import { endPoints } from "@/utils/api/route";
 import { getMethod } from "@/utils/api/getMethod";
 import { postMethod } from "@/utils/api/postMethod";
 import { signIn } from "next-auth/react";
+import { Button, Input } from "antd";
+import useLottieAnimation from "@/hooks/useAnimation";
+import animationData from "./loginAnimation.json"; 
+import { EyeInvisibleOutlined, EyeTwoTone } from '@ant-design/icons';
 
 const LoginComponent = () => {
   const dispatch = UseAppDispatch();
   const { email, password } = UseAppSelector(
     (state) => state?.loginauthReducer?.loginAuth?.loginInput
   );
+  const [passwordError, setPasswordError] = useState<string | null>(null);
+  const [isButtonDisabled, setIsButtonDisabled] = useState<boolean>(true);
 
   const handleChangeInput = (name: string, value: string) => {
     dispatch(handleChangeLoginInput(name, value));
   };
 
+  const validatePassword = (password: string): boolean => {
+    const passwordRegex = /^(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,}$/;
+    return passwordRegex.test(password);
+  };
+
+  const validateEmail = (email: string): boolean => {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+  };
+
+  useEffect(() => {
+    // Disable the button if email or password is invalid
+    if (
+      !validateEmail(email) ||
+      !validatePassword(password) ||
+      email === "" ||
+      password === ""
+    ) {
+      setIsButtonDisabled(true);
+    } else {
+      setIsButtonDisabled(false);
+    }
+  }, [email, password]);
+
   const handleLoginSubmit = async () => {
+    if (!validatePassword(password)) {
+      setPasswordError(
+        "Password must contain with uppercase letter, number, special character."
+      );
+      return;
+    }
+
+    setPasswordError(null);
+    
     try {
       const response = await postMethod({route:endPoints.auth.login, postData:{
-        email: email,
-        password: password
+        strEmail: email,
+        strPassword: password
       }},
     )
+    console.log("response", response);
+    
     if(response.data.statusCode === 200) {
       await signIn("credentials", {
         ...response?.data?.user,
@@ -39,45 +80,70 @@ const LoginComponent = () => {
       console.error(error)
     }
   }
-
+  const containerRef = useLottieAnimation({
+    animationData,
+    loop: true,
+    autoplay: true,
+  });
   return (
-    <div>
-      <div className=" w-full flex flex-col gap-4  ">
-        <div className=" flex flex-col gap-4 ">
-          <label htmlFor="email">Email:</label>
-          <input
-            type="email"
-            name="email"
-            title="email"
-            id="email"
-            placeholder="Enter your email"
-            value={email}
-            onChange={(e) => handleChangeInput("email", e.target.value)}
-            className=" px-2 py-3 rounded-md text-black "
-          />
-        </div>
-        <div className=" flex flex-col gap-4 ">
-          <label htmlFor="password">Password:</label>
-          <input
-            type="password"
-            name="password"
-            title="password"
-            id="password"
-            value={password}
-            placeholder="Enter your password"
-            onChange={(e) => handleChangeInput("password", e.target.value)}
-            className=" px-2 py-3 rounded-md text-black "
-          />
-        </div>
-        <div className=" w-full  ">
-          <button
-            type="button"
-            title="login"
-            className=" w-full bg-green-700 text-white p-2 rounded-md hover:bg-green-600  "
-            onClick={handleLoginSubmit}
-          >
-            Login
-          </button>
+    <div className="flex w-full min-h-screen justify-center items-center bg-gray-100">
+      {/* Lottie Animation Section */}
+      <div className="w-1/2 flex justify-center items-center">
+        <div ref={containerRef} className="w-96 h-96" />
+      </div>
+
+      {/* Login Form Section */}
+      <div className="w-[40%] p-8 bg-white shadow-lg rounded-lg">
+          <div className="flex justify-center font-semibold text-xl">Login for Pets?</div>
+        <div className="flex flex-col gap-4 px-4 py-6">
+          <div className="flex flex-col gap-4">
+            <label htmlFor="email">Email:</label>
+            <Input
+              type="email"
+              name="email"
+              title="email"
+              id="email"
+              placeholder="Enter your email"
+              value={email}
+              onChange={(e) => handleChangeInput("email", e.target.value)}
+              className="px-4 py-3 rounded-md text-black"
+              allowClear
+            />
+          </div>
+          <div className="flex flex-col gap-4">
+            <label htmlFor="password">Password:</label>
+            <Input.Password
+              type="password"
+              name="password"
+              title="password"
+              id="password"
+              value={password}
+              placeholder="Enter your password"
+              onChange={(e) => handleChangeInput("password", e.target.value)}
+              className="px-4 py-3 rounded-md text-black"
+              allowClear
+              iconRender={(visible) =>
+                visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />
+              }
+              visibilityToggle 
+            />
+            {passwordError && (
+              <p className="text-red-500 text-sm">{passwordError}</p>
+            )}
+          </div>
+          <div className="w-full">
+            <Button
+              type="button"
+              title="login"
+              className={`w-full bg-green-700 text-white p-2 rounded-md hover:bg-green-600 ${
+                isButtonDisabled ? "cursor-not-allowed bg-gray-400 hover:bg-gray-300" : "cursor-pointer"
+              }`}
+              onClick={handleLoginSubmit}
+              disabled={isButtonDisabled}
+            >
+              Login
+            </Button>
+          </div>
         </div>
       </div>
     </div>
